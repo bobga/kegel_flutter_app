b0f6a48164b2b0e5c06b15315a265742e2bc1704
mariyab96 <mariya.balabanova.96@gmail.com>
2021-02-02 15:43:28 +0300

    All UI for Learn Tab

---
+++ lib/pages/HowKegel.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class HowKegelWidget extends StatefulWidget {
+  final GlobalKey<ScaffoldState> parentScaffoldKey;
+
+  const HowKegelWidget({Key key, this.parentScaffoldKey}) : super(key: key);
+  @override
+  _HowKegelWidget createState() => _HowKegelWidget();
+}
+
+class _HowKegelWidget extends State<HowKegelWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(
+          color: Colors.black.withOpacity(0.5), //change your color here
+        ),
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(left: 30, top: 20, right: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "How to do Kegel?",
+              style: Theme.of(context).textTheme.headline1,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              "Kegel requires a rhythmic contraction of pelvic floor muscles to strengthen these muscles.",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              "- Get more intense orgasms.",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              "- Last longer in bed.",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Center(
+              child: Image.asset(
+                'assets/img/ic_tip_61.png',
+                width: MediaQuery.of(context).size.width * 0.7,
+              ),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  color: Color(0xFFFFF2F0),
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: SizedBox(
+                  width: 340,
+                  height: 70,
+                  child: Center(
+                    child: Text(
+                      'How to find pelvic floor muscles?',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 import 'pages/pages.dart';
 import 'pages/splash_screen.dart';
 import 'pages/DayExercise.dart';
+import 'pages/WhatsKegel.dart';
+import 'pages/WhatBenefits.dart';
+import 'pages/CanKegel.dart';
+import 'pages/HowKegel.dart';
+import 'pages/FindMuscles.dart';
+import 'pages/PleaseNote.dart';
+import 'pages/Disclaimer.dart';
+import 'pages/WhatBenefitsForWoman.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -12,6 +20,22 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (_) => SplashScreen());
       case '/DayExercise':
         return MaterialPageRoute(builder: (_) => DayExerciseWidget());
+      case '/WhatsKegel':
+        return MaterialPageRoute(builder: (_) => WhatsKegelWidget());
+      case '/WhatBenefits':
+        return MaterialPageRoute(builder: (_) => WhatBenefitsWidget());
+      case '/WhatBenefitsForWoman':
+        return MaterialPageRoute(builder: (_) => WhatBenefitsForWomanWidget());
+      case '/CanKegel':
+        return MaterialPageRoute(builder: (_) => CanKegelWidget());
+      case '/HowKegel':
+        return MaterialPageRoute(builder: (_) => HowKegelWidget());
+      case '/FindMuscles':
+        return MaterialPageRoute(builder: (_) => FindMusclesWidget());
+      case '/PleaseNote':
+        return MaterialPageRoute(builder: (_) => PleaseNoteWidget());
+      case '/Disclaimer':
+        return MaterialPageRoute(builder: (_) => DisclaimerWidget());
       case '/Pages':
         return MaterialPageRoute(builder: (_) => PagesWidget(currentTab: args));
       default:
